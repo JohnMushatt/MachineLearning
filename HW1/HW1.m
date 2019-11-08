95518fba22f5e11202f856b827085544f78b4065
John Mushatt <john.mushatt@gmail.com>
2019-11-08 01:55:30 -0500

    Added HW2 and finished 4c and 4d

--- HW1/HW1.m
+++ HW1/HW1.m
@@ -13,9 +13,10 @@ width = graph.BinWidth;
 %%area of the histogram
 area = sum_counts*width;
 
+normalized_data = normalize(data,'range');
 
 %---Problem 8---
-mle_normal = mle(data,'distribution','norm');
+mle_normal = mle(normalized_data,'distribution','norm');
 mean_norm = mle_normal(1);
 variance_normal = mle_normal(2);
 mean_and_variance = sprintf("MLE Mean: %f\nMLE Variance: %f",mean_norm,variance_normal);
@@ -23,38 +24,127 @@ text(0,.1,mean_and_variance);
 
 
 %---Problem 9---
-normalized_data = normalize(data,'range');
 %Scaled beta distribution
-data_beta = fitdist(normalized_data,'Beta');
+beta_dist = fitdist(normalized_data,'Beta');
 mle_beta = mle(normalized_data,'distribution','beta');
 mean_beta = mle_beta(1);
 variance_beta = mle_beta(2);
 
-
-beta_fit = betafit(normalized_data,mle_beta);
+alpha = 1.2404;
+beta = 0.9265;
+beta_xvals = -10:.01:10;
+scaled_beta=Scaled_BetaPDF(beta_xvals, alpha, beta, -10, 10);
+plot(beta_xvals, scaled_beta, 'LineWidth', 2);
 %Logistic distribution
-data_logistic = fitdist(data,'Logistic');
-mle_logistic = mle(normalized_data,'distribution','logistic');
+temp = normalized_data;
+indices= find(temp <= 0);
+temp(indices) = [];
+ab = any(temp <=0);
+
+data_logistic = fitdist(temp,'Lognormal');
+mle_logistic = mle(temp,'distribution','Lognormal');
 mean_logistic = mle_logistic(1);
 variance_logistic = mle_logistic(2);
 
 %---Problem 10---
 
-test = kfolds(data,10);
-function [test, train] = kfolds(data, k)
 
-  n = size(data,1);
+[m,n] = size(normalized_data) ;
 
-  test{k,1} = [];
-  train{k,1} = [];
+P = 0.90 ;
 
-  chunk = floor(n/k);
 
-  test{1} = data(1:chunk,:);
-  train{1} = data(chunk+1:end,:);
 
-  for f = 2:k
-      test{f} = data((f-1)*chunk+1:(f)*chunk,:);
-      train{f} = [data(1:(f-1)*chunk,:); data(f*chunk+1:end, :)];
-  end
+%normal
+log_data_normal = {0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0};
+count = 1;
+for test = 1:10
+    idx = randperm(m)  ;
+    Training = normalized_data(idx(1:round(P*m)),:) ;
+    Testing = normalized_data(idx(round(P*m)+1:end),:) ;
+    current_mle = mle(Training,'distribution','norm');
+    
+    mean_norm = current_mle(1);
+    
+    variance_normal = current_mle(2);
+    
+    current_pdf = makedist("Normal",mean_norm,variance_normal);
+    log_val = 0.0;
+    for index = 1:length(Testing)
+        
+        current_prob = pdf(current_pdf,Testing(index));
+        log_prob = log(current_prob);
+        
+        log_val= log_val + log_prob;
+    end
+    log_val = log_val / length(Testing);
+    fprintf("Current log value for normal dist: %d at index: %d\n",log_val,count);
+
+    log_data_normal{count} = log_val;
+    count = count + 1;
+end
+%beta
+log_data_beta = {0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0};
+count = 1;
+for test = 1:10
+    idx = randperm(m)  ;
+    Training = normalized_data(idx(1:round(P*m)),:) ;
+    Testing = normalized_data(idx(round(P*m)+1:end),:) ;
+    current_mle = mle(Training,'distribution','Beta');
+    
+    mean_beta = current_mle(1);
+    
+    variance_beta = current_mle(2);
+    
+    current_pdf = makedist("Beta",mean_beta,variance_beta);
+    log_val = 0.0;
+    for index = 1:length(Testing)
+        
+        current_prob = pdf(current_pdf,Testing(index));
+        log_prob = log(current_prob);
+        
+        log_val= log_val + log_prob;
+    end
+    log_val = log_val / length(Testing);
+    fprintf("Current log value for normal beta: %d at index: %d\n",log_val,count);
+
+    log_data_beta{count} = log_val;
+    count = count + 1;
+end
+%log
+log_data_log = {0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0};
+count = 1;
+[m,n] = size(temp) ;
+
+for test = 1:10
+    idx = randperm(m)  ;
+    Training = temp(idx(1:round(P*m)),:);
+    Testing = temp(idx(round(P*m)+1:end),:) ;
+    current_mle = mle(Training,'distribution','Lognormal');
+    
+    mean_log = current_mle(1);
+    
+    variance_log = current_mle(2);
+    
+    current_pdf = makedist("Lognormal",mean_log,variance_log);
+    log_val = 0.0;
+    for index = 1:length(Testing)
+        
+        current_prob = pdf(current_pdf,Testing(index));
+        log_prob = log(current_prob);
+        
+        log_val= log_val + log_prob;
+    end
+    log_val = log_val / length(Testing);
+    fprintf("Current log value for normal log: %d at index: %d\n",log_val,count);
+
+    log_data_log{count} = log_val;
+    count = count + 1;
+end
+figure;
+hold on;
+cellfun(@plot,log_data_normal,log_data_beta,log_data_log);
+
+function PDF = Scaled_BetaPDF(y, a, b, p, q)
+PDF = ( (y-p).^(a-1) .* (q - y).^(b-1) ) ./ ( (q - p).^(a+b-1) .* beta(a,b) );
 end
